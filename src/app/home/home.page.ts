aa0a43cbd123eab6834fd7c25c8c21247debdd92
Jorge Vergara <jorge@jorgevergara.co>
2022-07-03 23:11:23 -0400

    wip: Added modal

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonModal, IonRouterOutlet } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild(IonModal) modal: IonModal;
+  message = `Click Open to show the modal.`;
+  presentingElement = this.ionRouterOutlet.nativeEl;
+  constructor(private readonly ionRouterOutlet: IonRouterOutlet) {}
 
-  constructor() {}
+  dismiss() {
+    this.modal.dismiss(null, 'dismiss');
+  }
 
+  confirm() {
+    const person: Person = {
+      name: 'Jorge Vergara',
+      age: 36,
+    };
+    this.modal.dismiss(person, 'confirm');
+  }
+
+  onWillDismiss(modalEvent: Event) {
+    const customModalEvent = modalEvent as ModalCustomEvent;
+    if (customModalEvent.detail.role === 'dismiss') {
+      this.message = `User dismissed the modal, so nothing do to here.`;
+    }
+    if (customModalEvent.detail.role === 'confirm') {
+      const { name, age } = customModalEvent.detail.data;
+      this.message = `User confirmed their details, their name is ${name}, and they are ${age} years old.`;
+    }
+  }
+}
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+interface ModalCustomEvent extends CustomEvent {
+  target: HTMLIonModalElement;
+  detail: {
+    data: Person;
+    role: string;
+  };
 }
